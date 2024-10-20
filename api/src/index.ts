370b861e4b6a1a7040b95646b3ff718d45f8fb61
ortbri <brian.ort02@icloud.com>
2024-10-20 15:29:03 -0500

    jsobn parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,16 @@
 
 
-import express from "express";
+import express, {json, urlencoded} from "express";
 import productsRoutes from "./routes/products/index";
 
 const port = 3000;
 
 const app = express();
+// why do we need this?
+// middlewaresn if this is below /products, it will not work
+// if this is above /products, it will work
+app.use(urlencoded({extended: false}))
+app.use(json());
 
 app.use("/products", productsRoutes);
 
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -9,6 +9,9 @@ export function getProductById(req: Request, res: Response) {
     res.send("get product by id")
 }
 export function createProduct(req: Request, res: Response) { 
+
+    console.log(req.body)
+    
     res.send("create product")
 }
 export function updateProduct(req: Request, res: Response) { 
